5d62cc8a283b5c7f887980143bfb7bb74c6b7bdd
ricardozv <ricardosilvanet03@gmail.com>
2020-08-04 19:03:35 -0300

    yes

--- profi/src/routes.tsx
+++ profi/src/routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Route, { BrowserRouter } from 'react-router-dom';
+import Route, { BrowserRouter } from 'react-router-dom'
 
 
 import Landing from './pages/Landing';
