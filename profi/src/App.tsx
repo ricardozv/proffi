9377ab0608970ef3c8a92246e47188d558cab1f8
ricardozv <ricardosilvanet03@gmail.com>
2020-08-04 00:34:25 -0300

    yes

--- profi/src/App.tsx
+++ profi/src/App.tsx
@@ -4,20 +4,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-         Hell baby! 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Baby</h1>
     </div>
   );
 }
