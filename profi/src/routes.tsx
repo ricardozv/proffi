3ecd64bdeaa61535571451ffcbbd203d687f6a70
ricardozv <ricardosilvanet03@gmail.com>
2020-08-04 18:02:00 -0300

    yes

--- profi/src/routes.tsx
+++ profi/src/routes.tsx
@@ -7,9 +7,9 @@ function Routes() {
     return (
         <BrowserRouter>
         <Route path="/" exact component={Landing} />
-        <Route path="/study" component={TeacherList} />
-        <Route path="/give-classes" component={TeacherForm} />
-        </ BrowserRouter>
+        <Route  path="/study" component={TeacherList} />
+        <Route  path="/give-classes" component={TeacherForm} />
+        </BrowserRouter>
     )
 }
 
