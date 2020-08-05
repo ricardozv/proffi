24709d8c9f2c7e01c77d9379c7e3f0d2e73728ed
ricardozv <ricardosilvanet03@gmail.com>
2020-08-05 00:35:46 -0300

    yes

--- profi/src/components/PageHeader/index.tsx
+++ profi/src/components/PageHeader/index.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
-
 import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
 import backIcon from '../../assets/images/icons/back.svg';
 
-function PageHeader() {
+import './styles.css';
+
+interface PageHeaderProps {
+    title: string;
+}
+
+const PageHeader: React.FC<PageHeaderProps> = (props) => {
     return (
         <header className= "page-header">
-                <div className = "top-bar-container">
-                    <Link to="/">
+            <div className = "top-bar-container">
+                <Link to="/">
                         <img src={backIcon} alt="Voltar" />
-                    </Link>
-                    <img src={logoImg} alt="Proffy" />
-                    </div>
+                </Link>
+                        <img src={logoImg} alt="Proffy" />
+            </div>
 
                     <div className="header-content">
-                        <strong>Estes são os proffys disponíveis</strong>
+                        <strong>{props.title}</strong>
                     </div>
+                    {props.children}
             </header>
 
     );
